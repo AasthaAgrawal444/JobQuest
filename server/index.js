070243a0d9491a24f93d65fe433d62f0fd1edd3a
Aastha <asthaagrawal46@gmail.com>
2024-08-11 16:39:17 +0530

    aaded code

---
+++ server/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/userRoute.js"
+import companyRoute from "./routes/companyRoute.js";
+import jobRoute from "./routes/jobRoute.js";
+import applicationRoute from "./routes/applicationRoute.js";
+import path from "path";
+
+dotenv.config({});
+
+const __dirname = path.resolve();
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true,
+    methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req,res)=>{
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+})
+
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
